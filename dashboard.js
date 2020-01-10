3ac7c55c377b61c10806b5cac7066b4b7ccec033
mnicholas26 <spiritbeast26@yahoo.co.uk>
2020-01-10 11:45:01 +0000

    basic app layout and page functionality

--- dashboard.js
+++ dashboard.js
@@ -5,8 +5,8 @@ port.onMessage.addListener(function (msg)
     console.log(msg);
     if(msg == "setup")
     {
-        document.body.innerText = "ready";
-        titlesFromStorage();
+        //document.body.innerText = "ready";
+        //titlesFromStorage();
     }
     else if(msg == "titles updated")
     {
@@ -47,5 +47,37 @@ function clearTitles()
     while(document.body.firstChild)
     {
         document.body.removeChild(document.body.firstChild);
+    }
+}
+
+
+
+//page constuction and link up
+window.onload = setupPages
+
+function setupTabs(){
+
+}
+
+function setupPages()
+{
+    let sidebarbtns = document.querySelectorAll('#sidebars button');
+    let pages = document.querySelectorAll('.page');
+    let pageswrapper = document.getElementById('pages');
+    pageswrapper.pages = pages;
+    pageswrapper.currentpage = 0;
+    pages[0].classList.add("pagevisible");
+    //at least for time being (doesnt take into account settings page or in general any page not linked to tab)
+    for(let i = 0; i < sidebarbtns.length; i++)
+    {
+        sidebarbtns[i].addEventListener('click', () => {
+            if(pageswrapper.currentpage == i) return;
+            else 
+            {
+                pageswrapper.pages[pageswrapper.currentpage].classList.remove("pagevisible");
+                pages[i].classList.add("pagevisible");
+                pageswrapper.currentpage = i;
+            }
+        });
     }
 }
