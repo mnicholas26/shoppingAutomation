b6261bd6e958a3da4429fb2f74dc6702c5ae2db7
mnicholas26 <spiritbeast26@yahoo.co.uk>
2020-01-28 20:07:21 +0000

    refactored to work with template js generates html from template rather than pure rigid html

--- dashboard.js
+++ dashboard.js
@@ -40,29 +40,135 @@ function titlesFromStorage()
 
 
 //page constuction and link up
-window.onload = setupApp;
+window.onload = importTemplates;
 var tabs = [];
+var tabbtns = [];
 var currenttabindex = 0;
-var pages = [];
-var currentpageindex = 0;
+var templates = [];
+var templateindex = 0;
+var template = {};
 /*dev mode*/
 var dev = true;
 
-function setupApp()
+function importTemplates()
 {
-    //stuff
-    setupTabs();
-    let elem1 = document.getElementById('currenttest');
-    widgetFactory(widgetTitleTest, elem1);
+    chrome.storage.local.get({'templates': []}, (result) => {
+        if(result.templates.length > 0)
+        {
+            templates = result.templates;
+            chrome.storage.local.get({'templateindex': 0}, (result) => {
+                templateindex = result.templateindex;
+                //error checking
+                //if(templateindex > templates.length) templateindex = 0;
+                template = templates[templateindex];
+                setupApp()
+            } )
+        }
+        else
+        {
+            //do default
+            fetch('template.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsontemplate) => {
+                template = jsontemplate;
+                templates.push(template);
+                setupApp();
+            });
+        }
+    });
 }
 
+function setupApp()
+{
+    let tabbtnselem = document.getElementById('tabbtns');
+    let tabselem = document.getElementById('tabs');
+    //setup tabs
+    for(let i = 0; i < template.tabs.length; i++)
+    {
+        let tab = template.tabs[i];
+        let tabelem = document.createElement('div');
+        tabelem.classList.add('tab');
+        let tabbutton = document.createElement('button');
+        tabbutton.innerText = tab.title;
+        let sidebarelem = document.createElement('div');
+        sidebarelem.classList.add('sidebar');
+        let pageselem = document.createElement('div');
+        pageselem.classList.add('pages');
+        if(template.defaulttab == i)
+        {
+            tabelem.classList.add('tabvisible');
+            tabbutton.classList.add('activetabbtn');
+            currenttabindex = i;
+        }
+        tabelem.pages = []
+        tabelem.btns = [];
+        tabs.push(tabelem);
+        tabbtns.push(tabbutton);
+        //make pages
+        for(let j = 0; j < tab.pages.length; j++)
+        {
+            let page = tab.pages[j];
+            let pageelem = document.createElement('div');
+            pageelem.classList.add('page');
+            let pagebutton = document.createElement('button');
+            pagebutton.innerText = page.title;
+            if(tab.defaultpage == j)
+            {
+                pageelem.classList.add('pagevisible');
+                pagebutton.classList.add('activepagebtn');
+                tabelem.currentpageindex = j;
+            }
+            tabelem.pages.push(pageelem);
+            tabelem.btns.push(pagebutton);
+            pagebutton.addEventListener('click', () => {
+                if(tabelem.currentpageindex == j) return;
+                tabelem.pages[tabelem.currentpageindex].classList.remove("pagevisible");
+                pageelem.classList.add("pagevisible");
+                tabelem.btns[tabelem.currentpageindex].classList.remove("activepagebtn");
+                pagebutton.classList.add("activepagebtn");
+                tabelem.currentpageindex = j;
+            });
+            for(let k = 0; k < page.widgets.length; k++)
+            {
+                widgetFactory(window[page.widgets[k]], pageelem);
+            }
+            sidebarelem.appendChild(pagebutton);
+            pageselem.appendChild(pageelem);
+        }
+        tabbutton.addEventListener('click', () => {
+            if(i == currenttabindex) return;
+            tabs[currenttabindex].classList.remove("tabvisible");
+            tabelem.classList.add("tabvisible");
+            tabbtns[currenttabindex].classList.remove("activetabbtn");
+            tabbutton.classList.add("activetabbtn");
+            currenttabindex = i;
+        });
+        tabelem.appendChild(sidebarelem);
+        tabelem.appendChild(pageselem);
+        tabbtnselem.appendChild(tabbutton);
+        tabselem.appendChild(tabelem);
+    }
+    //setupTabs();
+}
+
+/*function setupApp()
+{
+    setupTabs();
+    setupWidgets();
+    //let elem1 = document.getElementById('currenttest');
+    //widgetFactory(widgetTitleTest, elem1);
+}*/
+
 function setupTabs(){
-    tabs = document.querySelectorAll('.tab');
+
+    /*tabs = document.querySelectorAll('.tab');
     setupTabBtns();
     for(let i = 0; i < tabs.length; i++)
     {
         setupPages(i);
-    }
+    }*/
 }
 
 function setupTabBtns()
